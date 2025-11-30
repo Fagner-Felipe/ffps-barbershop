f5123dffb190ca33530093711c4e40086e8049c8
unknown <FAGNER SANTIAGO@SMP-TEC-TI-008.sempre.sempre.net.br>
2025-11-30 12:10:09 -0300

    feat: add header and img components

---
+++ app/components/header.tsx
@@ -0,0 +1,15 @@
+import { MenuIcon } from "lucide-react";
+import { Button } from "./ui/button";
+
+const header = () => {
+  return (
+    <header className="flex items-center justify-between px-5 py-6">
+      <h1 className="text-2xl font-bold">Sua Barbearia</h1>
+      <Button variant="outline" size="icon">
+        <MenuIcon />
+      </Button>
+    </header>
+  );
+};
+
+export default header;
