7d2380e76231de8cd438c1416d3fd5c74c969aea
unknown <FAGNER SANTIAGO@SMP-TEC-TI-008.sempre.sempre.net.br>
2025-11-30 13:17:48 -0300

    feat: add booking item component

---
+++ app/components/booking-item.tsx
@@ -0,0 +1,62 @@
+import Image from "next/image";
+import { Badge } from "./ui/badge";
+import { Card } from "./ui/card";
+import { Avatar, AvatarImage } from "./ui/avatar";
+
+interface BookingItemProps {
+  // Define any props needed for the BookingItem component
+  serviceName: string;
+  barbershopeName: string;
+  barbershopeImageUrl: string;
+  date: Date;
+}
+
+const BookingItem = (props: BookingItemProps) => {
+  return (
+    <Card className="flex w-full min-w-full flex-row items-center justify-between p-0">
+      {/* Esquerda */}
+      <div className="flex flex-col gap-4 flex-1 p-6">
+        <Badge variant="default">Confirmado</Badge>
+        <div className="flex flex-col gap-2">
+          <p className="font-bold"> {props.serviceName}</p>
+          <div className="flex items-center gap-2">
+            <Avatar className="h-8 w-8">
+              <AvatarImage
+                src={props.barbershopeImageUrl}
+                alt={props.barbershopeName}
+                width={28}
+                height={28}
+                className="rounded-full"
+              />
+            </Avatar>
+            <p className="text-muted-foreground text-sm">
+              {props.barbershopeName}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Direita */}
+      <div className="flex flex-col items-center justify-center p-4 h-full border-l">
+        <p className="text-xs capitalize">
+          {props.date.toLocaleDateString("pt-br", {
+            month: "long",
+          })}
+        </p>
+        <p className="text-xs capitalize">
+          {props.date.toLocaleDateString("pt-br", {
+            day: "2-digit",
+          })}
+        </p>
+        <p className="text-xs capitalize">
+          {props.date.toLocaleTimeString("pt-br", {
+            hour: "2-digit",
+            minute: "2-digit",
+          })}
+        </p>
+      </div>
+    </Card>
+  );
+};
+
+export default BookingItem;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import Header from "./components/header";
 import SearchInput from "./components/search-input";
-import banner from "../public/banner.png";
+import barbershopBanner from "../public/barbershopBanner.jpg";
+import BookingItem from "./components/booking-item";
 
 const Home = () => {
   return (
@@ -10,10 +11,19 @@ const Home = () => {
       <div className="px-5 space-y-4">
         <SearchInput />
         <Image
-          src={banner}
+          src={barbershopBanner}
           alt="Agende agora"
           sizes="100vw"
-          className="w-full h-auto"
+          className="w-full h-auto rounded-2xl"
+        />
+        <h2 className="text-xs font-semibold text-foreground uppercase">
+          Agendamentos
+        </h2>
+        <BookingItem
+          serviceName="Corte de Cabelo"
+          barbershopeName="Barbearia do Fagner"
+          barbershopeImageUrl="https://utfs.io/f/45331760-899c-4b4b-910e-e00babb6ed81-16q.png"
+          date={new Date()}
         />
       </div>
     </>
